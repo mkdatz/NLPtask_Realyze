6d37a409e0c5e965458da79188fbc0c3091ca7ec
Michael Datz <mkdatz@gmail.com>
2021-03-17 21:16:22 -0400

    Add final

--- NLPtask.java
+++ NLPtask.java
@@ -1,22 +1,40 @@
 import java.io.*;
+import java.util.*;
 
 public class NLPtask {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		int wordCount = 0;
+		int sentenceCount = 0;
+		int paragraphCount = 1;
 		// take in user input from the command line
 		// this assumes they provide the full name with extension
 		Scanner userInput = new Scanner(System.in);
-		System.out.println("Provide a filename: ");
-		String userInput = in.nextLine();
-		File file = new File(input);
+		System.out.print("Provide a filename: ");
+		String answer = userInput.nextLine();
+		FileInputStream file = new FileInputStream(answer);
+		InputStreamReader input = new InputStreamReader(file);
+		BufferedReader parse = new BufferedReader(input);
+		String line;
 
-		// create a scanner that reads in the text file with the specified name
-		Scanner fileRead = new Scanner(new Filereader(file));
-		StringBuilder built = new StringBuilder();
-		while(fileRead.hasNext()) {
-			build.append(fileRead.next());
+		int i = 0;
+		while((line = parse.readLine()) != null) {
+			if(line.equals("")) {
+				paragraphCount++;
+			}
+			else {
+				String[] words = line.split("\\s+");
+				wordCount += words.length;
+
+				String[] sentences = line.split("  ");
+
+
+				System.out.println(sentences[i]);
+
+				sentenceCount += sentences.length;
+			}
 		}
-		// close the scanner and put the output to a full string
-		fileRead.close();
-		output = built.toString();
+		System.out.println("Total word count = " + wordCount); 
+        System.out.println("Total number of sentences = " + sentenceCount); 
+        System.out.println("Number of paragraphs = " + paragraphCount); 
 	}
 }
